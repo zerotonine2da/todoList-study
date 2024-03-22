5dc369c7c49994ba952473162145d605b2b453fb
zerotonine2da <96953412+zerotonine2da@users.noreply.github.com>
2024-03-22 18:25:20 +0900

    modify:투두리스트 화면

--- src/components/TodoContent.tsx
+++ src/components/TodoContent.tsx
@@ -1,10 +1,13 @@
 export default function TodoContent() {
     return (
-        <div className="border-solid border-2 border-indigo-600  ">
-            <h1>제목</h1>
-            <h3>내용</h3>
-            <button className="border-solid border-2 border-indigo-600 mr-5 ">삭제</button>
-            <button className="border-solid border-2 border-indigo-600 ">완료</button>
-        </div>
+        <>
+            <div>Todo </div>
+            <div className="border-solid border-2 border-indigo-600  ">
+                <h1>제목</h1>
+                <h3>내용</h3>
+                <button className="border-solid border-2 border-indigo-600 mr-5 ">삭제</button>
+                <button className="border-solid border-2 border-indigo-600 ">완료</button>
+            </div>
+        </>
     );
 }
